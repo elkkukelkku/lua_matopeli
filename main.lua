30249f1e2506ed75807805f1a0aeb10a53529249
Your Name <you@example.com>
2020-09-24 18:22:03 +0300

    mato liikkuu

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 --funktioita kutsutaan kerran pelin alussa
-
+--🤣
 function love.load ()
 	leveys = 20
 	korkeus = 15
@@ -26,9 +26,44 @@ function love.draw ()
 	
 	love.graphics.setColor(.28,.28,.28)
 	
-	love graphics.rectangle('fill',0,0,leveys*ruutu,korkeus*ruutu)
+	love.graphics.rectangle('fill',0,0,leveys*ruutu,korkeus*ruutu)
 	
 	local function piirraRuutu(x,y)
-		love graphics.rectangle('fill',(x-1)*ruutu,(y-1)*ruutu,ruutu-1,ruutu-1)
+		love.graphics.rectangle('fill',(x-1)*ruutu,(y-1)*ruutu,ruutu-1,ruutu-1)
+	end
+	
+	for indekksi,matopala in ipairs(mato) do
+		love.graphics.setColor(0.6,0.9,0.32)
+		piirraRuutu(matopala.x,matopala.y)
 	end
 end
+
+function love.update(dt)
+
+	local seuraavaX = mato[1].x
+	local seuraavaY = mato[1].y
+	
+	if suunta == 'right' then
+	
+		seuraavaX = seuraavaX + 1
+		
+		if seuraavaX > leveys then
+			suunta='left'
+		end
+		
+	elseif suunta == 'left' then
+	
+		seuraavaX=seuraavaX-1
+		
+		if seuraavaX < 1 then
+			suunta='right'
+		end
+	end
+	
+	table.insert(mato,1,{x = seuraavaX, y = seuraavaY})
+	table.remove(mato)
+	
+end
+
+
+
