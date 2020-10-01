5872aa1cce158fb18ecaa2c6bb369232319dfb0b
Your Name <you@example.com>
2020-10-01 18:25:29 +0300

    en ehtinyt pistää tähän mitään järkevää

--- main.lua
+++ main.lua
@@ -16,10 +16,11 @@ function reset ()
 	
 	suunta = 'right'
 	ajastin = 0
+	aikaraja = 0.15
 	
 end
 
---funktio peliframien piirtÃ¤miseen
+--funktio peliframien piirtämiseen
 function love.draw ()
 
 	local ruutu = 15
@@ -40,30 +41,70 @@ end
 
 function love.update(dt)
 
-	local seuraavaX = mato[1].x
-	local seuraavaY = mato[1].y
-	
-	if suunta == 'right' then
-	
-		seuraavaX = seuraavaX + 1
+	ajastin=ajastin+dt
+
+	if ajastin>=aikaraja then
 		
-		if seuraavaX > leveys then
-			suunta='left'
+		ajastin=ajastin-aikaraja
+	
+		local seuraavaX = mato[1].x
+		local seuraavaY = mato[1].y
+		
+		pelijatkuu=true
+	
+		if suunta == 'right' then
+			seuraavaX=seuraavaX+1
+			if seuraavaX > leveys -1 then
+				--gameover
+				pelijatkuu=false
+			end
+		
+		elseif suunta == 'left' then
+			seuraavaX=seuraavaX-1
+			if seuraavaX < 1 then
+				--gameover
+				pelijatkuu=false
+			end
+		
+		elseif suunta == 'down' then
+			seuraavaY=seuraavaY+1
+			if seuraavaY > korkeus-1 then
+				--gameover
+				pelijatkuu=false
+			end
+		
+		elseif suunta == 'up' then
+			seuraavaY=seuraavaY-1
+			if seuraavaY < 1 then
+				--gameover
+				pelijatkuu=false
+			end
+			
 		end
 		
-	elseif suunta == 'left' then
-	
-		seuraavaX=seuraavaX-1
-		
-		if seuraavaX < 1 then
-			suunta='right'
-		end
+		table.insert(mato,1,{x = seuraavaX, y = seuraavaY})
+		table.remove(mato)
 	end
-	
-	table.insert(mato,1,{x = seuraavaX, y = seuraavaY})
-	table.remove(mato)
-	
 end
+
+function love.keypressed(key)
+	if key=='right'and pelijatkuu and suunta~='left'then
+		suunta='right'
+	elseif key=='left'and pelijatkuu and suunta ~='right'then
+		suunta='left'
+	elseif key=='down'and pelijatkuu and suunta ~='up'then
+		suunta='down'
+	elseif key=='up'and pelijatkuu and suunta~='down'then
+		suunta='up'
+	end
+end
+
+
+
+
+
+
+
 
 
 
